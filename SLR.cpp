d0bc6e503bdca1c647af2732de328515d0fbc3ad
aaditya <nepalaaditya2002@gmail.com>
2023-12-05 11:46:58 +0100

    goto functions finished

--- SLR.cpp
+++ SLR.cpp
@@ -10,6 +10,16 @@ SLR::SLR() {
     StartSymbolSlr = this->getStartSymbol()+"'";
 }
 
+SLR::SLR(const CFG &cfg) {
+    this->setV(cfg.getVariables());
+    this->setP(cfg.getProductions());
+    this->setS(cfg.getStartSymbol());
+    this->setT(cfg.getTerminals());
+    augmented_production = this->getProductions();
+    augmented_production.push_back({this->getStartSymbol()+"'",{this->getStartSymbol()}});
+    StartSymbolSlr = this->getStartSymbol()+"'";
+}
+
 
 void SLR::closure() {
 
@@ -22,8 +32,10 @@ void SLR::closure() {
     variable_check(initial_i);
     goto_s.push_back(initial_i);
     CL = initial_i;
+    number_of_i++;
 }
 
+//hulpfunctie closure
 void SLR::variable_check(I &i0) {
     for(auto i : i0){
         auto it = find(this->getVariables().begin(), this->getVariables().end(), i.first.second[i.second]);
@@ -40,6 +52,7 @@ void SLR::variable_check(I &i0) {
     }
 }
 
+//checks if the parameter head is in i0(also i hulpfunctie for closure)
 bool SLR::production_checker(const string& head, I &i0) {
     for(const auto& i : i0) {
         if (i.first.first == head) {
@@ -48,65 +61,110 @@ bool SLR::production_checker(const string& head, I &i0) {
     }
     return false;
 }
+//constructs goto functions
+void SLR::goto_constructor() {
+    for (int i = 0; i < number_of_i; i++) {
+        i_vector_maker(goto_s[i]);
+        for (const auto &rule: goto_s[i]) {
+            // lopen door de body van elke rule in I
+            for (int k = 0; k < rule.first.second.size(); k++) {
+                // als de meegegeven variabele == variabele na puntje en als de variable niet goto function heeft, maak het aan
+                if (k == rule.second && i_vector_checker(rule.first.second[k],i)) {
+                    //kijk of de gegeven regel al bestaat
+                    pair<production,int> temp_rule = {rule.first,rule.second+1};
+                    if(!check_for_constructed_rules(temp_rule)){
+                        i_maker(goto_s[i],rule.first.second[k]);
+                        //remove the symbol from I_vector
+                        for(auto &i_vector : I_vector[i]){
+                            if(i_vector == rule.first.second[k]){
+                                I_vector[i].erase(std::remove(I_vector[i].begin(), I_vector[i].end(), i_vector));
+                            }
+                        }
 
-SLR::SLR(const CFG &cfg) {
-    this->setV(cfg.getVariables());
-    this->setP(cfg.getProductions());
-    this->setS(cfg.getStartSymbol());
-    this->setT(cfg.getTerminals());
-    augmented_production = this->getProductions();
-    augmented_production.push_back({this->getStartSymbol()+"'",{this->getStartSymbol()}});
-    StartSymbolSlr = this->getStartSymbol()+"'";
-}
-
-void SLR::goTo(const I &random_I, string variable, int number) {
-
-    // nieuwe I maken
-    I new_I;
-    // loopen door een meegegeven I (in het begin is het I0)
-    for (const auto &rule : random_I) {
-        // loopen door de body van elke rule in I
-        for (int i = 0; i < rule.first.second.size(); i++) {
-            // als de meegegeven variabele == variabele na puntje -> voeg toe aan new_I en verschuif puntje
-            if (variable == rule.first.second[i] && i == rule.second) {
-                int pos = rule.second + 1;
-                pair<production,int> new_var = {rule.first,pos};
-                new_I.push_back(new_var);
-            }
-        }
-
-        for (const auto &prod : new_I) {
-            auto it = find(this->getVariables().begin(), this->getVariables().end(), prod.first.second[prod.second]);
-            if (it != this->getVariables().end()){
-                for (const auto &rule : CL) {
-                    if (rule.first.first == *it) {
-                        new_I.push_back(rule);
                     }
                 }
             }
         }
-
     }
+}
 
-    for (auto &symbol : I_vector[number]) {
-        if (symbol == variable) {
-            I_vector[number].erase(std::remove(I_vector[number].begin(), I_vector[number].end(), variable), I_vector[number].end());
+//makes a vector for I_vector
+void SLR::i_vector_maker(const I &random_I) {
+    vector<string>temp;
+    for (const auto &rule : random_I) {
+        // lopen door de body van elke rule in I
+        for (int i = 0; i < rule.first.second.size(); i++) {
+            // als de meegegeven variabele == variabele na puntje -> voeg toe aan temp
+            auto it = find(temp.begin(),temp.end(),rule.first.second[i]);
+            if(it == temp.end()){
+                if (i == rule.second) {
+                    temp.push_back(rule.first.second[i]);
+                }
+            }
+        }
+    }
+    I_vector.push_back(temp);
+}
+
+//checks if a given symbol has a goto for given i
+bool SLR::i_vector_checker(string symbol, int number_i) {
+    for(const auto &k : I_vector[number_i]){
+        if(k == symbol){
+            return true;
+        }
+    }
+    return false;
+}
+
+//generates new i's
+void SLR::i_maker(const I &i, const string& symbol) {
+    I new_i;
+    for(const auto &j : i){
+        //check if the symbol comes after .
+        if(j.first.second[j.second] == symbol){
+            //change pos of .
+            int pos = j.second+1;
+            new_i.emplace_back(j.first,pos);
+            //check if the next symbol is a variable
+            auto it = find(this->getVariables().begin(), this->getVariables().end(),j.first.second[j.second+1]);
+            if(it != this->getVariables().end()){
+                closure_checker(new_i,j.first.second[j.second+1]);
+                rem_variables = {};
+            }
+
+        }
+    }
+    goto_s.push_back(new_i);
+    number_of_i++;
+}
+
+//recursive closure function, if we can apply closue on a rule then this function does it
+void SLR::closure_checker(I &new_I, const string& head) {
+    for(const auto& closure: CL){
+        if(closure.first.first == head){
+            new_I.push_back(closure);
+        }
+    }
+    rem_variables.push_back(head);
+    //check if we can apply closure in the just added rules
+    for(int i = 0; i < new_I.size(); i++){
+        auto it = find(rem_variables.begin(),rem_variables.end(),new_I[i].first.second[new_I[i].second]);
+        auto is_variable = find(this->getVariables().begin(), this->getVariables().end(),new_I[i].first.second[new_I[i].second]);
+        if(it == rem_variables.end() && is_variable != this->getVariables().end()){
+            closure_checker(new_I,new_I[i].first.second[new_I[i].second]);
         }
     }
 
 }
 
-void SLR::remaining_Variables(const I &random_I) {
-
-    for (auto symbol : random_I) {
-        vector<string> temp;
-        auto it = find(temp.begin(),temp.end(), symbol.first.second[symbol.second]);
-        if (it == temp.end()) {
-            temp.push_back(symbol.first.second[symbol.second]);
+//check if the create rule already exists
+bool SLR::check_for_constructed_rules(const pair<production,int> &check_i) {
+    for(const auto &i : goto_s){
+        for(const auto &rules : i){
+            if(equal(rules.first.second.begin(),rules.first.second.end(), check_i.first.second.begin()) && check_i.first.first == rules.first.first && check_i.second == rules.second){
+                return true;
+            }
         }
     }
-
-
-
-
+    return false;
 }
--- SLR.h
+++ SLR.h
@@ -8,6 +8,7 @@
 
 #include "CFG.h"
 #include "algorithm"
+#include "vector"
 using namespace std;
 using production = pair<string,vector<string>>;
 //production gives the production rule and int is for the position of dot
@@ -16,9 +17,13 @@ using I = vector<pair<production,int>>;
 class SLR : public CFG {
 private:
     vector<production> augmented_production;
+    int number_of_i = 0;
     vector<I> goto_s;
     string StartSymbolSlr;
+    //Collection of symbols which has not been implemented by goto
     vector<vector<string>> I_vector;
+    vector<string> rem_variables;
+    //closure
     I CL;
 public:
     SLR();
@@ -31,9 +36,17 @@ public:
 
     bool production_checker(const string& head, I& i0);
 
-    void goTo(const I &random_I, string variable, int number);
+    void i_vector_maker(const I &i);
 
-    void remaining_Variables(const I &random_I);
+    void goto_constructor();
+
+    bool i_vector_checker(string symbol, int number_i);
+
+    void closure_checker(I &new_I, const string& head);
+
+    void i_maker(const I& i, const string& symbol);
+
+    bool check_for_constructed_rules(const pair<production,int>  &check_i);
 
 };
 
